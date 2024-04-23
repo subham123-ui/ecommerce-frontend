94fdb46a9389169a4f2d1bce1f0f0fa215139b36
subham123-ui <suvam.rockstar@gmail.com>
2024-04-24 00:25:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,27 @@ const Search   =  lazy(()=>import("./pages/search"))
 const Cart =  lazy(()=>import("./pages/cart")) 
 
 
+// Admin Routes Importing
+
+const Dashboard = lazy(() => import("./pages/admin/dashboard"));
+const Products = lazy(() => import("./pages/admin/products"));
+const Customers = lazy(() => import("./pages/admin/customers"));
+const Transaction = lazy(() => import("./pages/admin/transaction"));
+const Barcharts = lazy(() => import("./pages/admin/charts/barcharts"));
+const Piecharts = lazy(() => import("./pages/admin/charts/piecharts"));
+const Linecharts = lazy(() => import("./pages/admin/charts/linecharts"));
+const Coupon = lazy(() => import("./pages/admin/apps/coupon"));
+const Stopwatch = lazy(() => import("./pages/admin/apps/stopwatch"));
+const Toss = lazy(() => import("./pages/admin/apps/toss"));
+const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
+const ProductManagement = lazy(
+  () => import("./pages/admin/management/productmanagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/admin/management/transactionmanagement")
+);
+
+
 
 const App = () => {
   return (
