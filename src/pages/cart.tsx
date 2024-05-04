ecf11b482bdc28a94bbd33558664320435bb4526
subham123-ui <suvam.rockstar@gmail.com>
2024-05-04 23:47:20 +0530

    Update cart.tsx

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import { VscError } from "react-icons/vsc";
+
+const cartitems = [];
+const subtotal = 40000;
+const tax = Math.round(subtotal * 0.18);
+const shippinCharges = 200;
+const total = subtotal + tax + shippinCharges;
+const discount = 400;
+
 const Cart = () => {
-  const cartitems = [];
-  const subtotal = 40000;
-  const tax = Math.round(subtotal * 0.18);
-  const shippinCharges = 200;
-  const total = subtotal + tax + shippinCharges;
-  const discount = 400;
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
   return (
     <div className="cart">
       <main></main>
@@ -18,6 +24,21 @@ const Cart = () => {
         <p>
           <b>Total: ₹{total} </b>
         </p>
+
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode && isValidCouponCode ? (
+          <span className="green">
+            ₹{discount} off using the <code>{couponCode}</code>{" "}
+          </span>
+        ) : (
+          <span className="red">Invalid Coupon <VscError/></span>
+        )}
       </aside>
     </div>
   );
